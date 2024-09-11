ab2e7f827dd51b0b451831a7fbda6899b94470f8
Blaise McNamara <blaise@outlook.com.au>
2024-09-11 15:27:07 +0800

    Initial Commit

---
+++ TouringAus/TouringAusApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouringAusApp.swift
+//  TouringAus
+//
+//  Created by Blaise McNamara on 11/9/2024.
+//
+
+import SwiftUI
+
+@main
+struct TouringAusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
